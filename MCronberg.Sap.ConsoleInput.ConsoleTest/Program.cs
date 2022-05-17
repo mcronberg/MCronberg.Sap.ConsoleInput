da88eae5dd6902241a2070ced37f250172bde04f
Michell <Admin@TA-822-INS>
2022-05-17 12:17:50 +0200

    publish

--- MCronberg.Sap.ConsoleInput.ConsoleTest/Program.cs
+++ MCronberg.Sap.ConsoleInput.ConsoleTest/Program.cs
@@ -2,27 +2,64 @@
 namespace MCronberg.Sap.ConsoleInput.ConsoleTest
 {
     using MCronberg.Sap.ConsoleInput.Core;
-
+    using System;
     using System.Collections.Generic;
 
     public class Program
-{
-    public static void Main(string[] args)
     {
-        var s1 = FileBinder.Bind<Settings>("appsettings.json");
-        var s2 = FileBinder.Bind<Settings, Root>("appsettingsWithRoot.json");
-        var s3 = ConsoleBinder.Bind<Settings>();
+        public static void Main(string[] args)
+        {
+            TestSimpleFileBinder();
+            TestSimpleFileBinderRoot();  
+            TestArgsBinder();
+            TestConsoleBinder();
 
-        // simulate args
-        List<string> a = new List<string>();
-        a.Add("-n");
-        a.Add("1");
-        a.Add("s");
-        a.Add("a");
-        a.Add("b");
-        a.Add("true");
-        var s4 = ArgumentsBinder.Bind<Settings>(a.ToArray());
+        }
 
+        private static void TestSimpleFileBinder()
+        {
+            var s = FileBinder.Bind<Settings>("appsettings.json");            
+            ShowJson(s, "Simple FileBinder");
+        }
+
+        private static void TestSimpleFileBinderRoot()
+        {
+            var s = FileBinder.Bind<Settings, Root>("appsettingsWithRoot.json");
+            ShowJson(s, "FileBinder with root");
+        }
+
+        private static void TestConsoleBinder()
+        {
+            var s = ConsoleBinder.Bind<Settings>();
+            Console.WriteLine();
+            ShowJson(s, "ConsoleBinder");
+        }
+
+        private static void TestArgsBinder()
+        {
+            // simulate args
+            List<string> a = new List<string>();
+            // -n 1 -s a -b true
+            a.Add("-n");
+            a.Add("1");
+            a.Add("s");
+            a.Add("a");
+            a.Add("b");
+            a.Add("true");
+            var s = ArgumentsBinder.Bind<Settings>(a.ToArray()); 
+            ShowJson(s, "ArgsBinder");
+        }
+
+        private static void ShowJson(Settings s, string title = null)
+        {
+            if (title != null)
+            {
+                Console.WriteLine(title);
+                Console.WriteLine(new string('-', title.Length));
+            }
+            string json = System.Text.Json.JsonSerializer.Serialize(s, options: new System.Text.Json.JsonSerializerOptions { WriteIndented = true });
+            Console.WriteLine(json);
+            Console.WriteLine();
+        }
     }
-}
 }
